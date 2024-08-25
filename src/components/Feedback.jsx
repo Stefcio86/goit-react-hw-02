b4a964f3759cf4c811d578d9ef124adf794c07d5
Stefcio86 <mstefansiu@gmail.com>
2024-08-25 17:57:21 +0200

    step1

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+const Feedback = ({ feedback }) => {
+    return (
+        <div>
+            <p>Good: {feedback.good}</p>
+            <p>Neutral: {feedback.neutral}</p>
+            <p>Bad: {feedback.bad}</p>
+        </div>
+    );
+};
+Feedback.PropTypes = {
+feedback: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+}).isRequired,
+};
+export default Feedback;
---
+++ src/components/Options.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const Options = ({ onLeaveFeedback}) => {
+    return (
+        <div>\
+            <button onClick={() => onLeaveFeedback("good")}>Good</button>
+            <button onClick={() => onLeaveFeedback("neutral")}>Neutral</button>
+            <button onClick={() => onLeaveFeedback("bad")}>Bad</button>
+        </div>
+    );
+};
+Options.propTypes = {
+    onLeaveFeedback: PropTypes.func.isRequired,
+
+};
+export default Options;
